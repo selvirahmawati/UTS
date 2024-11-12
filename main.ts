f8ff176ea65cfb8135b02fa257b75d4ae438b8f6
Rizki583 <rizungki583@gmail.com>
2024-11-12 15:10:38 +0700

    push all

---
+++ main.ts
@@ -0,0 +1,173 @@
+// //Latihan1
+// // console.log('Hallo World');
+// // var a = 10;
+// //  console.log(a);
+// //  var a = 20;                                       // <1>
+// //  console.log(a);
+// //  let b = 30;
+// //  console.log(b);
+// //  b = b + 5;                                         // <2>
+// //  // let b = b + 10;                               // <3>
+// //  const c = "softwareku";
+// //  console.log(c);
+// //  // c = 'softwareku - versi 1.0';         // <4>
+
+// //LATIHAN2
+// let isFinished: boolean = false;
+// console.log(isFinished, typeof isFinished);                           //  <1>
+// let price: number = 150.34;
+// console.log(price, typeof price);
+// let numOfEmployees: number = 25;
+// console.log(numOfEmployees, typeof numOfEmployees);
+// let progLang: string = "TypeScript";
+// console.log(progLang, typeof progLang)
+// let university: string[] = ['UT', 'UGM', 'ITB'];
+// console.log(university, typeof university)
+// let employee: [number, string, boolean, number, string];
+// employee = [1, "Zaky Aditya", true, 20, "Engineer"];
+// console.log(employee, typeof employee);
+// enum Color {
+//     Black = 2,
+//     Blue,
+//     Yellow,
+//     Green = 3,
+//     Red = 3 * 3
+// }
+// console.log(Color, typeof Color);
+// let code: string | number;
+// console.log(code, typeof code);
+// code = 'my code';
+// console.log(code, typeof code);
+// code = 21;
+// console.log(code, typeof code);
+// let valueNull = null
+// console.log(valueNull, typeof valueNull)
+// let valueUndefined = undefined
+// console.log(valueUndefined, typeof valueUndefined)
+// let valueAny: any;
+// console.log(valueAny, typeof valueAny)
+// valueAny = true;
+// console.log(valueAny, typeof valueAny)
+// valueAny = 42;
+// console.log(valueAny, typeof valueAny)
+// valueAny = 'TypeScript';
+// console.log(valueAny, typeof valueAny)
+// valueAny = [];
+// console.log(valueAny, typeof valueAny)
+// valueAny = {};
+// console.log(valueAny, typeof valueAny)
+// valueAny = Math.random;
+// console.log(valueAny, typeof valueAny)
+// valueAny = null;
+// console.log(valueAny, typeof valueAny)
+// valueAny = undefined;
+// console.log(valueAny, typeof valueAny)
+// let valueUnknown: unknown;
+// console.log(valueUnknown, typeof valueUnknown);
+// valueUnknown = true;
+// console.log(valueUnknown, typeof valueUnknown);
+// valueUnknown = 42;
+// console.log(valueUnknown, typeof valueUnknown);
+// valueUnknown = 'TypeScript';
+// console.log(valueUnknown, typeof valueUnknown);
+// valueUnknown = [];
+// console.log(valueUnknown, typeof valueUnknown);
+// valueUnknown = {};
+// console.log(valueUnknown, typeof valueUnknown);
+// valueUnknown = Math.random;
+// console.log(valueUnknown, typeof valueUnknown);
+// valueUnknown = null;
+// console.log(valueUnknown, typeof valueUnknown);
+// valueUnknown = undefined;
+// console.log(valueUnknown, typeof valueUnknown);
+
+// Pencabangan dengan if
+// let a: number = 21;
+// let b: number = 3;
+// let c:number = a / b;
+// if (c > 5) {
+// console.log("lebih besar dari 5");
+// } else if (c > 3) {
+// console.log("antar 3 - 5");
+// } else {
+// console.log("di bawah 3");
+
+// Seleksi Kondisi dengan switch
+// let hari: number = 5;
+//  let hariStr: string;
+//  switch (hari) {
+//  case 0:
+//    hariStr = "Minggu";
+//    break;
+//  case 1:
+//    hariStr = "Senin";
+//    break;
+//  case 2:
+//    hariStr = "Selasa";
+//    break;
+//  case 3:
+//    hariStr = "Rabu";
+//    break;
+//  case 4:
+//    hariStr = "Kamis";
+//    break;
+//    case 5:
+//     hariStr = "Jum’at";
+//     break;
+//   case 6:
+//     hariStr = "Sabtu";
+//     break;
+//   default:
+//     hariStr = "Tidak ada hari tersebut";
+//     break;
+//   }
+//   console.log(hariStr);
+
+//Perulangan dengan for
+// for (let i = 0; i < 3; i++) {                                 // <1>
+//     console.log("Looping ke " + i);
+//     }
+//     let arr = ["nilai 1", "nilai 2", "nilai 3", "nilai 4"];
+//     for (var nilai of arr) {                                       // <2>
+//     console.log(nilai);
+//     }
+//     for (var index1 in arr) {                                   // <3>
+//     console.log(index1);
+//     }
+//     console.log(index1);                                      // <4>
+//     for (let index2 in arr) {                                    // <5>
+//     console.log(index2);
+//     }
+//     // console.log(index2)                                    // <6>
+//     let str = "Universitas Terbuka";
+//     for (var huruf of str) {                                     // <7>
+//     console.log(huruf);
+//     }
+
+// //Perulangan dengan whil
+// let nilai1: number = 5;
+// while (nilai1 < 10) {                   // <1>
+//   console.log( nilai1 )
+//   nilai1++;
+// }
+// let nilai2: number = 5;
+// while (nilai2 < 5) {                     // <2>
+//   console.log( nilai2 )
+//   nilai2++;
+// }
+// let nilai3: number = 5;
+// do {                                           // <3>
+//     console.log( nilai3 )
+//     nilai3++;
+//   } while ( nilai3 < 10)
+//   let nilai4: number = 5;
+//   do {                                           // <4>
+//     console.log( nilai4 )
+//     nilai4++;
+//   } while ( nilai4 < 5)
+
+
+
+    
+        
+
